9d70626414b7967e393e6ca326d8cb3adb096431
tsabz <tonia.saba@gmail.com>
2020-10-27 19:13:51 -0400

    created react app for expence budget

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Header} from './components/Header';
+import {Balance} from './components/Balance';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
