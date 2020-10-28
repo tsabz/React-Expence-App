4d1fd44ada5823e015406adeb01e51b29000b25f
tsabz <tonia.saba@gmail.com>
2020-10-27 20:32:21 -0400

    able to render transaction.text from our transactionlist onto dom

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -2,15 +2,17 @@ import React, { useContext } from 'react'
 import {GlobalContext} from '../context/GlobalState'
 
 export const TransactionList = () => {
-    const context = useContext(GlobalContext);
-        console.log(context)
+    const {transactions} = useContext(GlobalContext);
+        console.log(transactions)
     return (
         <>
           <h3>History</h3>  
           <ul className="list">
+              {transactions.map(transaction =>(
               <li className="minus">
-                Cash <span>-$400</span><button className="delete-btn">x</button>
-              </li>
+                {transaction.text} <span>-$400</span>
+                <button className="delete-btn">x</button>
+              </li>))}
           </ul>
         </>
     )
