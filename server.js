b61c5bfb2c3f7e8327c145f753ed040951a118fd
tsabz <tonia.saba@gmail.com>
2020-10-28 18:11:59 -0400

    connected app to mongodb !!

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const express = require('express');
 const dotenv = require('dotenv'); // global variables
 const colors = require('colors');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
-dotenv.config({path: '.config/config.env'})
+dotenv.config({path: './config/config.env'});
+
+connectDB();
 
 const transactions = require('./routes/transactions')
 
